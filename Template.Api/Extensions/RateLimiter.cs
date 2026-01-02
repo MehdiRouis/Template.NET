c3f77fd4d1198f5c80ff2f4ccb48afe433f63ff3
AlexandreRomagnoli <alexandre-romagnoli@outlook.fr>
2026-01-02 11:29:12 +0100

    Refactor RateLimiter config & add it to dependency injection

---
+++ Template.Api/Extensions/RateLimiter.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace Template.Api.Extensions
+{
+    public static class RateLimiter
+    {
+        public static IServiceCollection AddRateLimiter(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            // ===== RateLimiter =====
+            services.AddRateLimiter(limiterOptions =>
+            {
+                limiterOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+                limiterOptions.AddFixedWindowLimiter("fixed", opt =>
+                {
+                    opt.Window = TimeSpan.FromSeconds(10);
+                    opt.PermitLimit = 3;
+                    opt.QueueLimit = 0;
+                });
+
+                limiterOptions.AddSlidingWindowLimiter("sliding", opt =>
+                {
+                    opt.Window = TimeSpan.FromSeconds(15);
+                    opt.SegmentsPerWindow = 3;
+                    opt.PermitLimit = 15;
+                    opt.QueueLimit = 0;
+                });
+
+                limiterOptions.AddTokenBucketLimiter("token", opt =>
+                {
+                    opt.TokenLimit = 20;
+                    opt.TokensPerPeriod = 5;
+                    opt.ReplenishmentPeriod = TimeSpan.FromSeconds(1);
+                    opt.QueueLimit = 0;
+                });
+
+                limiterOptions.AddConcurrencyLimiter("concurrency", opt =>
+                {
+                    opt.PermitLimit = 5;
+                    opt.QueueLimit = 0;
+                });
+            });
+
+            return services;
+        }
+    }
+}
--- Template.Api/Program.cs
+++ Template.Api/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
@@ -9,7 +8,7 @@ using Template.Api.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.RegisterServices(builder.Configuration);
-
+builder.Services.AddRateLimiter(builder.Configuration);
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -32,40 +31,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
         ClockSkew = TimeSpan.FromSeconds(10)
     };
-});
-
-builder.Services.AddRateLimiter(limiterOptions =>
-{
-    limiterOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
-
-    limiterOptions.AddFixedWindowLimiter("fixed", opt =>
-    {
-        opt.Window = TimeSpan.FromSeconds(10);
-        opt.PermitLimit = 3;
-        opt.QueueLimit = 0;
-    });
-
-    limiterOptions.AddSlidingWindowLimiter("sliding", opt =>
-    {
-        opt.Window = TimeSpan.FromSeconds(15);
-        opt.SegmentsPerWindow = 3;
-        opt.PermitLimit = 15;
-        opt.QueueLimit = 0;
-    });
-
-    limiterOptions.AddTokenBucketLimiter("token", opt =>
-    {
-        opt.TokenLimit = 20;
-        opt.TokensPerPeriod = 5;
-        opt.ReplenishmentPeriod = TimeSpan.FromSeconds(1);
-        opt.QueueLimit = 0;
-    });
-
-    limiterOptions.AddConcurrencyLimiter("concurrency", opt =>
-    {
-        opt.PermitLimit = 5;
-        opt.QueueLimit = 0;
-    });
 });
 
 var app = builder.Build();
